ce91b5506c282c4a742f2eb2e6a92fa119e11a99
xzmeasy <1280428201@qq.com>
2018-08-26 13:59:25 +0800

    create producer

---
+++ src/main/java/com/java/activemq/ActivemqProducer.java
@@ -0,0 +1,13 @@
+package com.java.activemq;
+
+/**
+ * @author xzmeasy
+ * @since 18-8-26
+ */
+public class ActivemqProducer {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/com/java/activemq/ActivemqProducerTest.java
@@ -0,0 +1,21 @@
+package com.java.activemq;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author xzmeasy
+ * @since 18-8-26
+ */
+public class ActivemqProducerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
